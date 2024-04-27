462d30e1305bf632ee99ed7784613f86cc41eb61
beneqo0 <158744573+beneqo0@users.noreply.github.com>
2024-04-27 10:35:37 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,73 @@
+# Import
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+def result_calculate(size, lights, device):
+    # Zmienne umożliwiające obliczenie poboru energii przez urządzenia
+    home_coef = 100
+    light_coef = 0.04
+    devices_coef = 5   
+    return size * home_coef + lights * light_coef + device * devices_coef 
+
+# Pierwsza strona
+@app.route('/')
+def index():
+    return render_template('index.html')
+# Druga strona
+@app.route('/cokolwiek')
+def cokolwiek():
+    return render_template(
+                            'jakos.html', 
+                            
+                           )
+@app.route('/<size>')
+def lights(size):
+    return render_template(
+                            'lights.html', 
+                            size=size
+                           )
+
+# Trzecia strona
+@app.route('/<size>/<lights>')
+def electronics(size, lights):
+    return render_template(
+                            'electronics.html',                           
+                            size = size, 
+                            lights = lights                           
+                           )
+
+# Obliczenia
+@app.route('/<size>/<lights>/<device>')
+def end(size, lights, device):
+    return render_template('end.html', 
+                            result=result_calculate(int(size),
+                                                    int(lights), 
+                                                    int(device)
+                                                    )
+                        )
+# Formularz
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+# Wyniki formularza
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    # Zadeklaruj zmienne do gromadzenia danych
+    name = request.form['name']
+    address = request.form['address'] 
+    email = request.form['email']
+    date = request.form['date']
+
+    # Możesz zapisać swoje dane lub wysłać je e-mailem
+    return render_template('form_result.html', 
+                           # Umieść tutaj zmienne
+                           name=name,
+                           date=date,
+                           address=address,
+                           email = email,
+                           )
+
+app.run(debug=True)
